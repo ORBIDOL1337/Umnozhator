f9c89af2ff8c163ae3dd4ec480b6a5295d137efd
ORBIDOL1337 <lolman383838@gmail.com>
2023-01-29 20:07:31 +0300

    Написал код для умножения чисел

---
+++ КлассУмножение.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        //
+        // TODO: Add constructor logic here
+        //
+        int Otvet;
+        Console.WriteLine("Введите первое число");
+        int Chislo1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int Chislo2 = Convert.ToInt32(Console.ReadLine());
+        Otvet = Chislo1 * Chislo2;
+        Console.WriteLine($"Результат умножения чисел:{Otvet}");
+        Console.ReadKey();з
+    }
+}
